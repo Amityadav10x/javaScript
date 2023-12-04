ed83c8c1aac0cec40e8556002f18732e02088d84
Amityadav10x <ay3040869@gmail.com>
2023-12-04 19:54:52 +0530

    save it

---
+++ 03_basic/01_functions.js
@@ -0,0 +1,34 @@
+console.log("h");
+console.log("i");
+console.log("t");
+console.log("e");
+console.log("s");
+
+
+function sayMyName () {
+console.log("h");
+console.log("i");
+console.log("t");
+console.log("e");
+console.log("s");
+
+}
+
+// sayMyName()
+
+// function addTwoNumbers (number1, number2) {
+
+//      console.log(number1 + number2 );
+// }
+
+function addTwoNumbers (number1, number2) {
+
+    let result = number1 + number2
+    return result
+}
+
+const result = addTwoNumbers(3,7)
+console.log("result:" , result);
+
+
+
